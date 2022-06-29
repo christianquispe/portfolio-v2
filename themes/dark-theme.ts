b30aad26f94bc851884212aa79d29434955bf447
Christian Quispe <christianquispecamasca@gmail.com>
2022-06-28 22:45:48 -0500

    chore: add custom theme

--- themes/dark-theme.ts
+++ themes/dark-theme.ts
@@ -3,7 +3,10 @@ import { createTheme } from "@nextui-org/react";
 export const darkTheme = createTheme({
   type: "dark",
   theme: {
-    colors: {}, // override dark theme colors
+    colors: {
+      primary: "#571D91",
+      secondary: "#0952A5",
+    }, // override dark theme colors
     shadows: {
       blue500: "#0952A5",
       purple500: "#571D91",
