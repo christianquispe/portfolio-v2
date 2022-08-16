f2a560dbda3ccc76cd109ef35010e685417bb843
Christian Quispe <christianquispecamasca@gmail.com>
2022-08-15 23:17:20 -0500

    chore: improve types

--- interfaces/me.ts
+++ interfaces/me.ts
@@ -1,4 +1,14 @@
+import { IconsId } from './components';
+
 export interface Project {
   name: string;
   description: string;
+}
+
+export type SkillLevel = "high" | "medium" | "low"
+
+export interface Skill {
+  name: string;
+  level: SkillLevel;
+  icon: IconsId | React.ReactElement;
 }
